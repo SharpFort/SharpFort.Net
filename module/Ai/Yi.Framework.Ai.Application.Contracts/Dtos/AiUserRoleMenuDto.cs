fa9da27f1bf08fc0df8a0d688545d56209cbdfea
松鼠 <hhhelong@126.com>
2026-02-15 11:31:12 +0800

    feat(Ai): Remove Fuwuhao binding logic

--- module/Ai/Yi.Framework.Ai.Application.Contracts/Dtos/AiUserRoleMenuDto.cs
+++
@@ -1,9 +0,0 @@
-using Volo.Abp.Application.Dtos;
-using Yi.Framework.Rbac.Domain.Shared.Dtos;
-
-namespace Yi.Framework.Ai.Application.Contracts.Dtos;
-
-public class AiUserRoleMenuDto : UserRoleMenuDto
-{
-
-}
--- module/Ai/Yi.Framework.Ai.Application/Services/AiAccountService.cs
+++ module/Ai/Yi.Framework.Ai.Application/Services/AiAccountService.cs
@@ -1,4 +1,4 @@
-using Mapster;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SqlSugar;
@@ -6,10 +6,9 @@ using System.Globalization;
 using System.Text;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Users;
-using Yi.Framework.Ai.Application.Contracts.Dtos;
-using Yi.Framework.Ai.Domain.Entities;
 using Yi.Framework.Rbac.Application.Contracts.IServices;
 using Yi.Framework.Rbac.Domain.Shared.Dtos;
+using Yi.Framework.Ai.Domain.Entities;
 using Yi.Framework.SqlSugarCore.Abstractions;
 
 
@@ -18,15 +17,12 @@ namespace Yi.Framework.Ai.Application.Services;
 public class AiAccountService : ApplicationService
 {
     private IAccountService _accountService;
-    private ISqlSugarRepository<AiUserExtraInfoEntity> _userRepository;
     private ISqlSugarRepository<ChatMessage> _messageRepository;
     public AiAccountService(
         IAccountService accountService,
-        ISqlSugarRepository<AiUserExtraInfoEntity> userRepository,
         ISqlSugarRepository<ChatMessage> messageRepository)
     {
         _accountService = accountService;
-        _userRepository = userRepository;
         _messageRepository = messageRepository;
     }
 
@@ -36,16 +32,11 @@ public class AiAccountService : ApplicationService
     /// <returns></returns>
     [Authorize]
     [HttpGet("account/ai")]
-    public async Task<AiUserRoleMenuDto> GetAsync()
+    public async Task<UserRoleMenuDto> GetAsync()
     {
         var userId = CurrentUser.GetId();
         var userAccount = await _accountService.GetAsync();
-        var output = userAccount.Adapt<AiUserRoleMenuDto>();
-
-        // 是否绑定服务号
-        output.IsBindFuwuhao = await _userRepository.IsAnyAsync(x => userId == x.UserId);
-
-        return output;
+        return userAccount;
     }
 
 }
